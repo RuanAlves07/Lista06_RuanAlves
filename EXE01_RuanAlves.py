08ad0e61caa5e6322647dafb80ffc60a356bef03
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-02-21 21:06:01 -0300

    att

---
+++ EXE01_RuanAlves.py
@@ -0,0 +1,7 @@
+paises = ("Costa do marfim","Pais da gales","Holanda","Noruega","Gabão")
+
+print (paises)
+pais = input("Insira um dos paises: ")
+
+for i, pais in enumerate(paises):
+    print("indice: {}".format(pais))
